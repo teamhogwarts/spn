e2ad3d091b7aebb99c0e4071d73fda243c497b3e
Benjamin Brodwolf <benjamin.brodwolf@students.fhnw.ch>
2019-04-10 18:19:35 +0200

    - added Gitignore - refactoring Testcases

--- src/test/java/SPNTest.java
+++ src/test/java/SPNTest.java
@@ -8,32 +8,32 @@ class SPNTest {
     void sboxTest(){
         //given
         SPN spn = new SPN();
-        int actual[] = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-        int expected[] = new int[]{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7};
+        int[] actual = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        int[] expected = new int[]{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7};
 
 
         //when
-
+        int[] spnSBOXED = spn.sbox(actual);
 
         //then
-        assertEquals(spn.sbox(actual).length, 16);
-        assertArrayEquals(spn.sbox(actual), expected);
+        assertEquals(spnSBOXED.length, 16);
+        assertArrayEquals(spnSBOXED, expected);
     }
 
     @Test
     void bitpermutationTest(){
         //given
         SPN spn = new SPN();
-        int actual[] = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
-        int expected[] = new int[]{0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15};
+        int[] actual = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        int[] expected = new int[]{0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15};
 
 
         //when
-
+        int[] spnBP = spn.bitpermutation(actual);
 
         //then
-        assertEquals(spn.bitpermutation(actual).length, 16);
-        assertArrayEquals(spn.bitpermutation(actual), expected);
+        assertEquals(spnBP.length, 16);
+        assertArrayEquals(spnBP, expected);
     }
 
 }
